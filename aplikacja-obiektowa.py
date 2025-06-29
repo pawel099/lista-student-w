98d3e551511983bea53134d49510e6dafc6a3858
pawel123 <pawelek78-30@o2.pl>
2025-06-29 13:01:27 +0200

    lista studentów

---
+++ aplikacja-obiektowa.py
@@ -0,0 +1,95 @@
+import sys
+
+class Students:
+
+ def lista_all_students(self):
+     
+     student = {}
+     search = int(input(''))
+
+     match search:
+      case 1:
+        podaj_pesel = int(input("podaj pesel : "))
+      case 2:
+       return "liczba studentów na liście " , len(student)
+
+     for dane in student.keys():
+       if podaj_pesel == student.get(dane):
+         return dane
+       else:
+         return "nic nie odnaleziono " 
+
+print("wpisz 1 aby wyszukać studenta lub 2 by wyświetlić listę zapisanych")
+student = Students()
+
+full= student.lista_all_students()
+
+if full==None:
+  print('lista jest pusta')
+else:
+ print(full)
+
+
+ 
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+ 
+
+  
+
+
+
+ 
+
+
+    
+
+
+
+      
+     
+     
+
+
+
+
+
+      
+ 
+     
+       
+ 
+
+ 
+
+     
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
